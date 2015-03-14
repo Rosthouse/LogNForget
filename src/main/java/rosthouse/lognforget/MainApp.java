ffe49fa11554b825b24357eb33f3cb6ee4ccddf5
Rosthouse <rosthouse@gmail.com>
2015-03-14 15:19:45 +0100

    Refactored methods into own class

--- src/main/java/rosthouse/lognforget/MainApp.java
+++ src/main/java/rosthouse/lognforget/MainApp.java
@@ -21,14 +21,14 @@ import rosthouse.lognforget.shortcut.ShortCutListener;
 
 public class MainApp extends Application {
 
-    private static final String LOG_N_FORGET = "LogNForget";
+    public static final String LOG_N_FORGET = "LogNForget";
 
     @Override
     public void start(Stage stage) throws Exception {
         Platform.setImplicitExit(false);
         JIntellitype instance = JIntellitype.getInstance();
         initializeShortCutListener(instance);
-        initializeSystemTray();
+        Tray tray = new Tray();
     }
 
     private void initializeShortCutListener(JIntellitype instance) {
@@ -36,11 +36,6 @@ public class MainApp extends Application {
         listener.registerHotKeyForLogging(JIntellitype.MOD_WIN, 'A');
     }
 
-    private void initializeSystemTray() {
-        if (SystemTray.isSupported()) {
-            setTrayIcon();
-        }
-    }
 
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
@@ -53,48 +48,4 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
-    private void setTrayIcon() {
-        SystemTray tray = SystemTray.getSystemTray();
-        TrayIcon icon = loadTrayIcon();
-        PopupMenu menu = createPopupMenu(tray, icon);
-        icon.setPopupMenu(menu);
-        try {
-            tray.add(icon);
-        } catch (AWTException ex) {
-            Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    private TrayIcon loadTrayIcon() {
-        Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/com/famfamfam/silk/accept.png"));
-        TrayIcon icon = new TrayIcon(image, LOG_N_FORGET);
-        return icon;
-    }
-
-    private PopupMenu createPopupMenu(SystemTray tray, TrayIcon icon) throws HeadlessException {
-        PopupMenu menu = new PopupMenu(LOG_N_FORGET);
-        MenuItem options = new MenuItem("Options");
-        options.addActionListener((e) -> {
-            Platform.runLater(() -> {
-                try {
-                    Options optionsWindow = new Options();
-                } catch (IOException ex) {
-                    Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            });
-        });
-
-        MenuItem close = new MenuItem("Exit");
-        close.addActionListener((e) -> {
-            System.out.println("Ending " + LOG_N_FORGET);
-            JIntellitype.getInstance().cleanUp();
-            tray.remove(icon);
-            Platform.exit();
-        });
-        menu.add(options);
-        menu.add(close);
-        return menu;
-    }
-
 }
---
+++ src/main/java/rosthouse/lognforget/Tray.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rosthouse.lognforget;
+
+import com.melloware.jintellitype.JIntellitype;
+import java.awt.AWTException;
+import java.awt.HeadlessException;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import rosthouse.lognforget.options.Options;
+
+/**
+ *
+ * @author Rosthouse <rosthouse@gmail.com>
+ */
+public class Tray {
+
+    public Tray() {
+        if (SystemTray.isSupported()) {
+            setTrayIcon();
+        } else {
+            throw new UnsupportedOperationException("System Tray is not supported on this machine");
+        }
+    }
+
+    private void setTrayIcon() {
+        SystemTray tray = SystemTray.getSystemTray();
+        TrayIcon icon = loadTrayIcon();
+        PopupMenu menu = createPopupMenu(tray, icon);
+        icon.setPopupMenu(menu);
+        try {
+            tray.add(icon);
+        } catch (AWTException ex) {
+            Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private TrayIcon loadTrayIcon() {
+        Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/com/famfamfam/silk/accept.png"));
+        TrayIcon icon = new TrayIcon(image, MainApp.LOG_N_FORGET);
+        return icon;
+    }
+
+    private PopupMenu createPopupMenu(SystemTray tray, TrayIcon icon) throws HeadlessException {
+        PopupMenu menu = new PopupMenu(MainApp.LOG_N_FORGET);
+        MenuItem options = new MenuItem("Options");
+        options.addActionListener((ActionEvent e) -> {
+            openOptionsWindow();
+        });
+        MenuItem close = new MenuItem("Exit");
+        close.addActionListener((ActionEvent e) -> {
+            closeApplication(tray, icon);
+        });
+        menu.add(options);
+        menu.add(close);
+        return menu;
+    }
+
+    private void openOptionsWindow() {
+        Platform.runLater(() -> {
+            try {
+                Options optionsWindow = new Options();
+            } catch (IOException ex) {
+                Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+    }
+
+    private void closeApplication(SystemTray tray, TrayIcon icon) {
+        System.out.println("Ending " + MainApp.LOG_N_FORGET);
+        JIntellitype.getInstance().cleanUp();
+        tray.remove(icon);
+        Platform.exit();
+    }
+
+}
